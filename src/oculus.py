31939a7ed124d9bb1f183b4b2d104cc4347600ed
Kamil Zak <kamilzak00@gmail.com>
2020-09-26 14:52:45 +0100

    refactored code - fixed a few things.

--- src/oculus.py
+++ src/oculus.py
@@ -2,88 +2,73 @@ import requests
 from bs4 import BeautifulSoup
 
 
-class Oculus_Olx:
-    def olx_model_data(soup):
-        try:
-            model = soup.find('h1').text.strip()
-        except:
-            model = ""
-        try:
-            p = soup.find('div', class_="pricelabel").text.strip()
-            price, currency = p.split(' ')
-        except:
-            price = ""
-        
-        data = {'model' : model, 'price': price}
-        return data
-
-    def olx_index(soup):
-        try:
-            links = soup.find_all('a', class_="marginright5")
-        except:
-            links = []
-        urls = [link.get('href') for link in links]
-        return urls
-
-
-class Oculus_Allegro:
-    def allegro_model_data(soup):
-        try:
-            model = soup.find('h1').text.strip()
-        except:
-            model = ""
-        try:
-            price = soup.find('div', class_="_1svub _lf05o _9a071_2MEB_")
-        except:
-            price = ""
-        
-        data = {'model' : model, 'price': price}
-        return data
+class Content:
+    def __init__(self, model, price, url):
+        self.model = model
+        self.price = price
+        self.url = url
     
+    def print(self):
+        print(f"Model: {self.model} - Price: {self.price}")
 
-    def allegro_index(soup):
-        try:
-            links = soup.find_all('a', class_="_w7z6o _uj8z7 meqh_en mpof_z0 mqu1_16 _9c44d_2vTdY")
-        except:
-            links = []
-        urls = [link.get('href') for link in links]
-        return urls
+
+class Website:
+    def __init__(self, name, url, searchURL, resultListing, resultURL, absoluteURL, modelTag, priceTag):
+        self.name = name
+        self.url = url
+        self.searchURL = searchURL
+        self.resultListing = resultListing
+        self.resultURL = resultURL
+        self.absoluteURL = absoluteURL
+        self.modelTag = modelTag
+        self.priceTag = priceTag
 
 
 class Main:
-    def get_page(url):
+    def getPage(self, url):
         response = requests.get(url)
         if not response.ok:
             print('Server responded:', response.status_code)
         else:
-            soup = BeautifulSoup(response.text, 'lxml')
-        return soup
+            return BeautifulSoup(response.text, 'lxml')
 
-    def main():
-        olx_models = ["https://www.olx.pl/oferty/q-olympus-trip/",
-        "https://www.olx.pl/oferty/q-olympus-xa/",
-        "https://www.olx.pl/oferty/q-olympus-mju/",
-        "https://www.olx.pl/oferty/q-yashica-t/",
-        "https://www.olx.pl/oferty/q-konica-big-mini/"]
+    def safeGet(self, pageObj, selector):
+        childObj = pageObj.select(selector)
+        if childObj is not None and len(childObj) > 0:
+            return childObj[0].get_text()
+        return ''
 
-        allegro_models = ["https://allegro.pl/listing?string=olympus%20trip", 
-        "https://allegro.pl/listing?string=olympus%20xa",
-        "https://allegro.pl/listing?string=olympus%20mju",
-        "https://allegro.pl/listing?string=yashica%20t",
-        "https://allegro.pl/listing?string=konica%20big%20mini"]
+    def search(self, model, site):
+        bs = self.getPage(site.searchURL + model)
+        searchResults = bs.select(site.resultListing)
+        for result in searchResults:
+            url = result.select(site.resultURL)[0].attrs['href']
+            if (site.absoluteURL):
+                bs = self.getPage(url)
+            else:
+                bs = self.getPage(site.url + url)
+            if bs is None:
+                print("Error.")
+                return
+            model = self.safeGet(bs, site.modelTag)
+            price = self.safeGet(bs, site.priceTag)
+            if model != "" and price != "":
+                content = Content(model, price, url)
+                content.print()
 
-        print("OLX: \n")
-        for model in olx_models:
-            products = Oculus_Olx.olx_index(Main.get_page(model))
-            for link in products:
-                data = Oculus_Olx.olx_model_data(Main.get_page(link))
-                print(data)
-        
-        print("\nALLEGRO: \n")
-        for model in allegro_models:
-            products = allegro_index(Main.get_page(model))
-            for link in products:
-                data = allegro_model_data(Main.get_page(link))
-                print(data)
+main = Main()
 
-Main.main()
+siteData = [
+['OLX', 'https://www.olx.pl/', 'https://www.olx.pl/oferty/q-', 'div.offer-wrapper', 'h3.margintop5 a', 'True', 'h1', 'div.pricelabel'],
+['Allegro', 'https://allegro.pl/', 'https://allegro.pl/listing?string=', 'div.mpof_ki myre_zn _9c44d_1Hxbq', 'h2.mgn2_14 m9qz_yp mqu1_16 mp4t_0 m3h2_0 mryx_0 munh_0 mp4t_0 m3h2_0 mryx_0 munh_0 a', 'True', 'h1', 'div._1svub _lf05o _9a071_2MEB_']
+]
+
+sites = []
+for row in siteData:
+    sites.append(Website(row[0], row[1], row[2],
+                         row[3], row[4], row[5], row[6], row[7]))
+
+models = ['Olympus XA', 'Yashica T', 'Konica Big Mini']
+for model in models:
+    for targetSite in sites:
+        main.search(model, targetSite)
