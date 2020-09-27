7d1e3791a8e3160b0e8557eec881502080dd59a6
Kamil Zak <kamilzak00@gmail.com>
2020-09-27 17:28:44 +0100

    added a few comments, fixed bug with recommended items when given item does not exist (on OLX).

--- src/oculus.py
+++ src/oculus.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 
 class Content:
+    """Contains all variables/methods related to given content on a single website."""
     def __init__(self, model, price, url):
         self.model = model
         self.price = price
@@ -13,6 +14,7 @@ class Content:
 
 
 class Website:
+    """Contains all necessary variables related to a particular website."""
     def __init__(self, name, url, searchURL, resultListing, resultURL, absoluteURL, modelTag, priceTag):
         self.name = name
         self.url = url
@@ -25,9 +27,12 @@ class Website:
 
 
 class Main:
+    """Contains the main logic of the program, including scraping the given websites."""
     def __init__(self):
         self.load_data()
 
+
+    # Sends a request to the given url with a header, if response is OK - return object with lxml parser.
     def getPage(self, url):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'}
         response = requests.get(url, headers=headers)
@@ -36,30 +41,38 @@ class Main:
         else:
             return BeautifulSoup(response.text, 'lxml')
 
+    # Checks whether parsed selector exists, if exists return if not return empty string.
     def safeGet(self, pageObj, selector):
         childObj = pageObj.select(selector)
         if childObj is not None and len(childObj) > 0:
             return childObj[0].get_text()
         return ''
 
+    # Specific camera model and site are parsed into search,
+    # then are searched for and if model and price variables are
+    # not empty - return them via Content's print() method.
     def search(self, model, site):
         bs = self.getPage(site.searchURL + model)
         searchResults = bs.select(site.resultListing)
-        for result in searchResults:
-            url = result.select(site.resultURL)[0].attrs['href']
-            if (site.absoluteURL):
-                bs = self.getPage(url)
-            else:
-                bs = self.getPage(site.url + url)
-            if bs is None:
-                print("Error.")
-                return
-            model = self.safeGet(bs, site.modelTag).strip()
-            price = self.safeGet(bs, site.priceTag).strip()
-            if model != "" and price != "":
-                content = Content(model, price, url)
-                content.print()
+        if site.name == "OLX" and bs.find("div", {"class": "emptynew"}):
+            return ""
+        else:
+            for result in searchResults:
+                url = result.select(site.resultURL)[0].attrs['href']
+                if (site.absoluteURL):
+                    bs = self.getPage(url)
+                else:
+                    bs = self.getPage(site.url + url)
+                if bs is None:
+                    print("Error.")
+                    return
+                model = self.safeGet(bs, site.modelTag).strip()
+                price = self.safeGet(bs, site.priceTag).strip()
+                if model != "" and price != "":
+                    content = Content(model, price, url)
+                    content.print()
     
+    # Load specific camera models from data/camera_models.txt
     def load_data(self):
         global models
         try:
