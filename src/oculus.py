55a25cc0b13b690e789e30bc17c90c267e3baee7
Kamil Zak <kamilzak00@gmail.com>
2020-09-29 12:03:17 +0100

    added back allegro, fixed some bugs, added a rate limiting option (temp with time.sleep), added code for monitor (does not automatically work yet when search is done)

--- src/oculus.py
+++ src/oculus.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 from bs4 import BeautifulSoup
 
 
@@ -38,7 +39,11 @@ class Main:
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'}
         response = requests.get(url, headers=headers)
         if not response.ok:
-            print('Server responded:', response.status_code)
+            if response.status_code == 429:
+                print("Rate limiting (allegro) - waiting 7 seconds...\n")
+                time.sleep(7) # very unpythonic - will think of an alternative soon
+            elif response.status_code != 429:
+                print('Server responded:', response.status_code)
         else:
             return BeautifulSoup(response.text, 'lxml')
 
@@ -60,25 +65,25 @@ class Main:
         if site.name == "OLX" and bs.find("div", {"class": "emptynew"}):
             return ""
         else:
-            if searchResults:
-                for result in searchResults:
-                    url = result.select(site.resultURL)[0].attrs['href']
-                    if (site.absoluteURL):
-                        bs = self.getPage(url)
-                    else:
-                        bs = self.getPage(site.url + url)
-                    if bs is None:
-                        print("Error.")
-                        return
-                    model = self.safeGet(bs, site.modelTag).strip()
-                    model_price = self.safeGet(bs, site.priceTag).strip()
-                    price, *args = model_price.split(' ') # was getting an error if *args == currency, will re-factor later on.
-                    if model != "" and price != "" and int(price) <= camera['max_price']:
-                        content = Content(model, price, url) # splitting price and currency (*args) to parse price as int to set a max price for each product.
-                        content.print()
-            else:
-                main.monitor_stock(site)
-    
+            for result in searchResults:
+                url = result.select(site.resultURL)[0].attrs['href']
+                if (site.absoluteURL):
+                    bs = self.getPage(url)
+                else:
+                    bs = self.getPage(site.url + url)
+                if bs is None:
+                    return
+                model = self.safeGet(bs, site.modelTag).strip()
+                model_price = self.safeGet(bs, site.priceTag).strip()
+                model_price = model_price.replace(' ', '')
+                if site.name == "OLX":
+                    price, currency = model_price.split('zł')
+                elif site.name == "Allegro":
+                    price, *args = model_price.split(',')
+                if model != "" and price != "" and int(price) <= camera['max_price']:
+                    content = Content(model, price, url)
+                    content.print()
+
     # Load specific camera models from camera_config.json
     def load_data(self):
         global camera_config # will remove soon
@@ -95,7 +100,8 @@ class Main:
 
     def load_site_data(self):
         siteData = [
-        ['OLX', 'https://www.olx.pl/', 'https://www.olx.pl/oferty/q-', 'div.offer-wrapper', 'h3.margintop5 a', 'True', 'h1', 'div.pricelabel']
+        ['OLX', 'https://www.olx.pl/', 'https://www.olx.pl/oferty/q-', 'div.offer-wrapper', 'h3.margintop5 a', 'True', 'h1', 'div.pricelabel'],
+        ['Allegro', 'https://allegro.pl/', 'https://allegro.pl/listing?string=', 'div.mpof_ki.mqen_m6.mp7g_oh.mh36_0.mvrt_0.mg9e_8.mj7a_8.m7er_k4', 'h2.mgn2_14.m9qz_yp.mqu1_16.mp4t_0.m3h2_0.mryx_0.munh_0.mp4t_0.m3h2_0.mryx_0.munh_0 a', 'True', 'h1', 'div._1svub._lf05o._9a071_2MEB_']
         ]
 
         sites = []
@@ -113,7 +119,11 @@ class Main:
 
     def monitor_stock(self, site):
         print("No cameras found... Monitoring!")
-
+        for camera in camera_config['cameras']:
+            bs = self.getPage(site.searchURL + camera['model'])
+            new_camera_listing = bs.find("h3", {"class": "margintop5"})
+            if new_camera_listing is not None:
+                main.search(camera['model'], site)
 
 
 if __name__ == '__main__':
